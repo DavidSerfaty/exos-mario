421290a3a7d968fa38e1f754c726599fb5d05304
DavidSerfaty <david@owanted.io>
2020-09-26 10:43:26 +0200

    exo 5

---
+++ exo_5.rb
@@ -0,0 +1,57 @@
+# #{} sert à éxécuter du code Ruby à l'intérieur d'une string
+
+# Renvoi ce qu'il y a à l'intérieur des string à l'écran
+puts "On va compter le nombre d'heures de travail à THP"
+#  ==> On va compter le nombre d'heures de travail à THP
+
+#Renvoi la string avec la multiplication de ce qu'il y a entre les accolades
+puts "Travail : #{10 * 5 * 11}"
+#  ==> Travail : 550
+
+# Renvoi la string avec la multiplication de ce qu'il y a entre les accolades
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#  ==> En minutes ça fait : 33000
+
+# Renvoi ce qu'il y a à l'intérieur des string à l'écran
+puts "Et en secondes ?"
+#  ==> Et en secondes ?
+
+# Renvoi uniquement la multiplication de tous les chiffes
+puts 10 * 5 * 11 * 60 * 60
+#  ==> 1980000
+
+# Renvoi ce qu'il y a à l'intérieur des string à l'écran
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#  ==> Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?
+
+# Comparaison en Ruby qui renvoi un booleen
+puts 3 + 2 < 5 - 7
+#  ==> false
+
+# Renvoi la string avec l'addition de ce qu'il y a entre les accolades
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#  ==> Ça fait combien 3 + 2 ? 5
+
+# # Renvoi la string avec la soustraction de ce qu'il y a entre les accolades
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#  ==> Ça fait combien 5 - 7 ? -2
+
+# Renvoi ce qu'il y a à l'intérieur des string à l'écran
+puts "Ok, c'est faux alors !"
+#  ==> Ok, c'est faux alors !
+
+# Renvoi ce qu'il y a à l'intérieur des string à l'écran
+puts "C'est drôle ça, faisons-en plus :"
+#  ==> C'est drôle ça, faisons-en plus :
+
+# Renvoi la string avec le booleen de ce qu'il y a entre les accolades
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#  ==> Est-ce que 5 est plus grand que -2 ? true
+
+# Renvoi la string avec le booleen de ce qu'il y a entre les accolades
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#  ==> Est-ce que 5 est supérieur ou égal à -2 ? true
+
+# Renvoi la string avec le booleen de ce qu'il y a entre les accolades
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#  ==> Est-ce que 5 est inférieur ou égal à -2 ? false
